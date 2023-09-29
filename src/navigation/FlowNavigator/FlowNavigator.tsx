ddca40c59192a58a98a76ad4f723db184f7caa28
Charlotte Isambert <charlotte.isambert@gmail.com>
2023-09-29 15:28:07 +0200

    handle navigation by useEffect

--- src/navigation/FlowNavigator/FlowNavigator.tsx
+++ src/navigation/FlowNavigator/FlowNavigator.tsx
@@ -1,11 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Step1Navigator } from "./Step1Navigator/Step1Navigator";
 import { Step2Navigator } from "./Step2/Step2Navigator";
-import { StateMachineContext, StateMachineMachineProvider } from "../../stateMachine/StateMachineProvider";
+import {
+  StateMachineContext,
+  StateMachineMachineProvider,
+} from "../../stateMachine/StateMachineProvider";
 import { Step3Navigator } from "./Step3/Step3Navigator";
 import { Step4Navigator } from "./Step4/Step4Navigator";
 import { StateMachineProviderWrapper } from "../../stateMachine/StateMachineProviderWrapper";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 
 export type FlowStackParamList = {
   Step1Navigator: undefined;
@@ -24,18 +28,44 @@ export const FlowNavigator = () => {
   );
 };
 
+const PageNameByStep = {
+  step1: "Step1Navigator",
+  step2: "Step2Navigator",
+  step3: "Step3Navigator",
+  step4: "Step4Navigator",
+};
 
-const FlowPages = () => {
+export const FlowPages = () => {
   const { value } = useContext(StateMachineContext);
 
-  switch(value){
-    case 'step1':
-      return <Step1Navigator/>
-    case 'step2':
-      return <Step2Navigator/>
-    case 'step3':
-      return <Step3Navigator/>
-    case 'step4':
-      return <Step4Navigator/>
-  }
-}
+  const navigation = useNavigation<NavigationProp<FlowStackParamList>>();
+
+  useEffect(() => {
+    navigation.navigate(PageNameByStep[value]);
+  }, [value]);
+
+  return (
+    <FlowStack.Navigator initialRouteName="Step1Navigator">
+      <FlowStack.Screen
+        name="Step1Navigator"
+        component={Step1Navigator}
+        options={{ headerShown: false }}
+      />
+      <FlowStack.Screen
+        name="Step2Navigator"
+        component={Step2Navigator}
+        options={{ headerShown: false }}
+      />
+      <FlowStack.Screen
+        name="Step3Navigator"
+        component={Step3Navigator}
+        options={{ headerShown: false }}
+      />
+      <FlowStack.Screen
+        name="Step4Navigator"
+        component={Step4Navigator}
+        options={{ headerShown: false }}
+      />
+    </FlowStack.Navigator>
+  );
+};
